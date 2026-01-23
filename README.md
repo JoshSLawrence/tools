9a90133de3607fb95c03b8e9ec7094f856bdce83
Josh Lawrence <josh@joshlawrence.dev>
2026-01-22 19:29:59 -0500

    feat: add execution orchestrator at project root

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Tools
 
-Random tools I find helpful
+Random tools I made and find helpful.
+
+## Usage
+
+> [!TIP]
+> See "All Tools" below for internal links to each tool's README for details and
+> execution instructions.
+
+You can use the `tool_select.sh` script to find, select, and
+execute your tool of choice.
+
+```bash
+git clone https://github.com/JoshSLawrence/tools
+cd tools
+chmod +x ./tool_select.sh
+./tool_select.sh
+```
+
+## All Tools
 
 - [Entra App Redirect Fetcher](./microsoft/entra-app-redirect-fetcher/README.md)
---
+++ tool_select.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TOOL_REGISTRY="registry.json"
+
+# Target this (script's) directory
+cd "$(dirname "$0")"
+
+# Prompt user for tool selection
+tool=$(cat $TOOL_REGISTRY | jq ".[].name" | fzf)
+
+# Determine how to run selected tool
+LOCATION=$(cat $TOOL_REGISTRY | jq -r ".[] | select(.name == $tool) | .location")
+COMMAND=$(cat $TOOL_REGISTRY | jq -r ".[] | select(.name == $tool) | .command")
+ARGS=$(cat $TOOL_REGISTRY | jq -r ".[] | select(.name == $tool) | .args.[]")
+
+# If tool takes args, prompt for arg values
+PROCESSED_ARGS=""
+for arg in $ARGS; do
+  read -p "Value for $arg (press enter to skip): " value
+  if [ -n "$value" ]; then
+    PROCESSED_ARGS+="$arg $value "
+  fi
+done
+
+# Tool execution
+$COMMAND $LOCATION $PROCESSED_ARGS
